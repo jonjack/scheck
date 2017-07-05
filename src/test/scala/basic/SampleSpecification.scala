4811806eacca98d532931aa118b7a310ee408d33
jonjack <jonathan.x.jackson@gmail.com>
2017-07-05 12:54:49 +0100

    Updated project description

--- src/test/scala/basic/SampleSpecification.scala
+++ src/test/scala/basic/SampleSpecification.scala
@@ -9,14 +9,34 @@ object SampleSpecification extends Properties("A sample test specification") {
   property("testList") = forAll { (l1: List[Int], l2: List[Int]) =>
     l1.size + l2.size == (l1 ::: l2).size
   }
+  
+  property("Concatenated string starts with") = forAll { (a: String, b: String) => (a+b).startsWith(a) }
+  
+  val concatStringStrtsWith = forAll { (a: String, b: String) => (a+b).startsWith(a) }
+  
+  val concatStringStrtsWith2 = forAll { (s1: String, s2: String) => collect(s1, s2) { (s1 + s2).startsWith(s1) } }
+  
+  /*
+  property("check integer set threshold") = forAll { (iset: Set[Int]) =>
+    l1.size + l2.size == (l1 ::: l2).size
+  }
+  *
+  */
+  
+  //val propConcatLists = forAll { (l1: List[Int], l2: List[Int]) => l1.size + l2.size == (l1 ::: l2).size }
+  
+  
 
   /**
    * See https://github.com/oscarrenalias/scalacheck-cookbook/blob/master/markdown/analyzing-test-data.md
    */
+  /*
   property("Check concatenated string") = forAll { (a:String, b:String) =>
     collect(a, b) {
       a.concat(b) == a + b
     }
   }
+  * 
+  */
 
 }
